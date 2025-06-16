260b55754777fa7d163365209a873e6384b495ce
divyanshu035 <divyanshuShrivastav7@gmail.com>
2025-06-16 22:08:41 +0530

    routes try and catch

--- src/app.js
+++ src/app.js
@@ -2,16 +2,26 @@ const express = require("express");
 
 const app = express();
 
+// app.use("/tesst", (req, res) => {
+//     res.send("this is test");
+// });
+
+// app.use("/serve", (req, res) => {
+//     res.send("this is server");
+// });
+
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params)
+  res.send({ firstName: "Divyanshu", lastName: "Srivastava" });
+});
+
+app.post("/user", (req,res) => {
+    res.send("Post call successfully completed");
+})
+
 app.use("/", (req, res) => {
   res.send("Hello from the server");
 });
-
-
-app.use("/test", (req, res) => {
-    res.send("this is test");
-  });
-
-
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+app.listen(3001, () => {
+  console.log(`Server is running on port 3001`);
 });
